2a737e1601d0a346bc658d662a94d3b92cbf4307
Pavel Ivanov <grammka@gmail.com>
2022-12-23 14:59:39 +0100

    initial setup

---
+++ pages/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Games from './Games/Games'
+
+
+const App: React.FC = () => {
+
+  return (
+    <div>
+      <Games />
+    </div>
+  )
+}
+
+export default App
---
+++ pages/App/Games/Games.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Games: React.FC = () => {
+
+  return (
+    <div>Games</div>
+  )
+}
+
+export default Games
